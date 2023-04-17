d7cb669cdd33a9e389a63b5af7c6622e910c692d
petri <pvcoetzee@gmail.com>
2023-04-17 12:25:04 +0200

    Starting out with the Posts endpoint

--- src/app/Http/Controllers/PostsController.php
+++ src/app/Http/Controllers/PostsController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
+use App\Lib\Posts\PostRepository;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PostsController extends Controller
 {
-    //
+    /**
+     * @param string $code
+     * @return PostResource
+     */
+    public function getPost(string $code) : PostResource
+    {
+        $post = PostRepository::getByCode($code);
+
+        if (!$post->exists) {
+            throw new NotFoundHttpException();
+        }
+
+        return new PostResource($post);
+    }
+
+    public function createPost(Request $request)
+    {
+
+    }
+
+    public function editPost(Request $request)
+    {
+
+    }
+
+    public function deletePost(Request $request)
+    {
+
+    }
 }
--- src/app/Lib/Posts/PostRepository.php
+++ src/app/Lib/Posts/PostRepository.php
@@ -4,5 +4,16 @@ namespace App\Lib\Posts;
 
 class PostRepository
 {
-    // Something like getById() perhaps?
+    /**
+     * @param string $code
+     * @return Post
+     */
+    public static function getByCode(string $code) : Post
+    {
+        return Post::query()
+            ->where('code', '=', $code)
+            ->limit(1)
+            ->get()
+            ->first() ?? new Post();
+    }
 }
